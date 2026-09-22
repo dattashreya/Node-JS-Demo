f9c228c6a9ee3a109a8938a4833f526d9d01d0ac
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-09-22 13:39:39 +0530

    app-server-demo
    app-server-demo in js

---
+++ app-server-demo.js
@@ -0,0 +1,10 @@
+// console.log("Hello, World!");
+
+const http = require("http");
+
+http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello, World!\n");
+}).listen(3000, () => {
+  console.log("Server running at http://localhost:3000/");
+});
